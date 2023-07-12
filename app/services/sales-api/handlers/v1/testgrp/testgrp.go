523bdd2f38e8a4a3079b514af734dfc0ba469d4e
bricelalu <brice.lalu@free.fr>
2023-07-12 17:14:06 +0200

    feat: Add Error middleware

--- app/services/sales-api/handlers/v1/testgrp/testgrp.go
+++ app/services/sales-api/handlers/v1/testgrp/testgrp.go
@@ -2,12 +2,19 @@ package testgrp
 
 import (
 	"context"
+	"errors"
+	"math/rand"
 	"net/http"
 
+	v1 "github.com/bricelalu/service/business/web/v1"
 	"github.com/bricelalu/service/foundation/web"
 )
 
 func Status(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
+	if n := rand.Intn(100); n%2 == 0 {
+		return v1.NewRequestError(errors.New("trusted error"), http.StatusBadRequest)
+	}
+
 	status := struct{ Status string }{Status: "ok"}
 
 	return web.Respond(ctx, w, status, http.StatusOK)
