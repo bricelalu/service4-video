d5fe6f016f6f86c5ed7a6f469ae0c8cce1a01cfd
bricelalu <brice.lalu@free.fr>
2023-07-14 15:30:14 +0200

    fix: write public key to stdout instead of writing in files

--- app/scratch/jwt/main.go
+++ app/scratch/jwt/main.go
@@ -31,15 +31,38 @@ func run() error {
 	defer privateFile.Close()
 
 	// Construct a PEM block for the private key.
-	privateBlock := pem.Block{
-		Type:  "PRIVATE KEY",
-		Bytes: x509.MarshalPKCS1PrivateKey(privateKey),
-	}
+	// privateBlock := pem.Block{
+	// 	Type:  "PRIVATE KEY",
+	// 	Bytes: x509.MarshalPKCS1PrivateKey(privateKey),
+	// }
 
 	// Write the private key to the private key file.
-	if err := pem.Encode(privateFile, &privateBlock); err != nil {
-		return fmt.Errorf("encoding to private file: %w", err)
+	// if err := pem.Encode(privateFile, &privateBlock); err != nil {
+	// 	return fmt.Errorf("encoding to private file: %w", err)
+	// }
+
+	// =========================================================================
+
+	// Marshal the public key from the private key to PKIX.
+	asn1Bytes, err := x509.MarshalPKIXPublicKey(&privateKey.PublicKey)
+	if err != nil {
+		return fmt.Errorf("marshaling public key: %w", err)
 	}
+
+	// Construct a PEM block for the public key.
+	publicBlock := pem.Block{
+		Type:  "PUBLIC KEY",
+		Bytes: asn1Bytes,
+	}
+
+	fmt.Println("================================================")
+
+	// Write the public key to the public key file.
+	if err := pem.Encode(os.Stdout, &publicBlock); err != nil {
+		return fmt.Errorf("encoding to public file: %w", err)
+	}
+
+	fmt.Println("================================================")
 
 	return nil
 }
